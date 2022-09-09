42fbd2a669691f58412439c7830194ed598f7c98
kstrickland0612 <kstrickland0612@gmail.com>
2022-09-09 16:38:53 -0400

    K => Kai

--- src/components/working-with-me.tsx
+++ src/components/working-with-me.tsx
@@ -5,14 +5,14 @@ export const WorkingWithMe = () => (
     <h2 style={{ paddingTop: 20 }}>What it's like to work with me</h2>
     <div style={{ display: "flex", flexDirection: "column" }}>
       <p>
-        "K is the epitome of a team player. They have an ease of personality
+        "Kai is the epitome of a team player. They have an ease of personality
         that allows them to get along with everyone, paired with an honest moral
         compass; they are a team member who is looking out for the good of
         everyone in the organization, and working to ensure that workplaces are
-        a safe and inclusive atmosphere for all. K leads by inspiring others to
+        a safe and inclusive atmosphere for all. Kai leads by inspiring others to
         be the best they can be and helping them to reach their goals, while
         working hard to better themselves along the way. If you have a problem,
-        K will have a myriad of creative and thoughtful solutions. It was an
+        Kai will have a myriad of creative and thoughtful solutions. It was an
         honor and a pleasure to work with K, and I can say with absolute
         confidence that any organization would surely benefit from their
         positive presence, skills, and commitment."
@@ -21,21 +21,21 @@ export const WorkingWithMe = () => (
         <i>Natashia Ayres - Shipping Operations Specialist, Addgene</i>
       </div>
       <p>
-        "Working with K is so wonderful because they bring so many things to an
+        "Working with Kai is so wonderful because they bring so many things to an
         organization's mission and to a team: passion, commitment, a
         collaborative spirit, creativity, innovation, intelligence,
         perseverance, kindness, compassion. I have learned so much working with
-        K - most outstanding is what it means to jump into something you don't
+        Kai - most outstanding is what it means to jump into something you don't
         know, and to figure out how to achieve it. Anything that is given to K,
-        they tackle. They are someone I can depend on, someone I can trust. K is
-        also so fun to work with. All of this together makes K an amazing person
+        they tackle. They are someone I can depend on, someone I can trust. Kai is
+        also so fun to work with. All of this together makes Kai an amazing person
         to have on your team."
       </p>
       <div style={{ alignSelf: "flex-end", paddingBottom: 15 }}>
         <i>Sara Minkara - Founder & CEO, Empowerment Through Integration</i>
       </div>
       <p>
-        "In my 10+ years of working at Addgene, K was easily one of the
+        "In my 10+ years of working at Addgene, Kai was easily one of the
         sharpest, hardest working employees with whom I've worked. Most
         employees are either "big picture" people or "detail oriented", but K
         could transition between both states with apparent ease. Whether they
