1ccde5ec0d18493426dcd9a7ddd1dfaba5295dba
kstrickland0612 <kstrickland0612@gmail.com>
2024-08-06 08:21:09 -0400

    Updates

--- src/components/working-with-me.tsx
+++ src/components/working-with-me.tsx
@@ -5,12 +5,79 @@ export const WorkingWithMe = () => (
     <h2 style={{ paddingTop: 20 }}>What it's like to work with me</h2>
     <div style={{ display: "flex", flexDirection: "column" }}>
       <p>
+        "Since working with Kai, I feel like I have been empowered to an even
+        greater degree. Normally Product Managers are seen as the "Force
+        Multiplier" for the business, but I feel like working with Kai has been
+        the opposite. Kai doesn't shy away from a meeting or conversation with
+        anyone, and always asks great clarifying questions. They will even go so
+        far as to scope out tickets for me and lead the team through heavy
+        technical discussions, and have saved me so much time and allowed me to
+        focus efforts elsewhere that I have had to put off. To me, this serves
+        as a great example of a leader, as they are working to help people
+        outside the confines of those they collaborate with on a daily basis.
+        Kai also maintains an EXTREMELY positive attitude about seemingly
+        everything. When things are shit, and everyone is stressed, Kai always
+        finds a way to keep themselves positive, and focused on the things that
+        are going well. I feel that this kind of attitude becomes contagious,
+        and it forces people to do the same, and I even find myself feeling a
+        little more positive knowing I have Kai on my side coming into work, and
+        things won't be so bad as a result. Lastly, Kai feels like a good
+        person, more than just a co-worker. I never feel anxiety or any negative
+        emotions whenever I get a DM from Kai, and I'm always excited to answer
+        because I know Kai is either going to have an insightful
+        thought/question/idea about work, or be willing to joke around to
+        lighten the mood, and banter a bit back and forth, but never crosses any
+        boundaries or lines."
+      </p>
+      <div style={{ alignSelf: "flex-end", paddingBottom: 15 }}>
+        <i>Ian Griesdorn - Product Manager, Own Up</i>
+      </div>
+      <p>
+        "Kai is a supremely motivated individual with whom I've had the pleasure
+        of working ever since they were hired out of a programming bootcamp way
+        back in the summer of 2019. Kai levels up their knowledge quickly,
+        leaving no stone unturned. This has served them very well as tech lead:
+        the thoroughness and detail with which they prepare documentation and
+        break down the work into tasks, and the manner in which they comport
+        themselves when disseminating the information to other engineers. This
+        instills a lot of confidence in the team because they understand the
+        work, and they know that Kai's got their back. More generally, Kai is an
+        absolute rock for the team. They're very reliable and responsive, and do
+        a wonderful job of balancing delivery of story items against their
+        other, higher level obligations. They're good about asking for help when
+        they need it; and they're happy to pair with others to unblock them. I
+        want to call out Kai's attentiveness towards matters of inclusivity.
+        While we were porting the main marketing site from Wordpress, Kai
+        identified tooling to test for and familiarized themselves with
+        standards for accessibility, and helped make them part of the best
+        practices and CI/CD for our most public-facing frontends. During our
+        hackathon week, they successfully implemented customer-facing tooling to
+        collect user pronouns to promote social inclusion. I admire Kai for
+        their incredible thoughtfulness, and their willingness and ability to
+        meet others where they are."
+      </p>
+      <div style={{ alignSelf: "flex-end", paddingBottom: 15 }}>
+        <i>Liz Lorenz - Engineering Manager, Own Up</i>
+      </div>
+      <p>
+        "Kai is an absolute shining star on the development team. I have never
+        met someone with so much passion and initiative to take on such in-depth
+        research in order to identify solutions for problems raised to them,
+        even outside of their own responsibilities! Their advocacy for the
+        partnership between design and development has been critical to the
+        success of the design system, and I could not have done it without
+        them!"
+      </p>
+      <div style={{ alignSelf: "flex-end", paddingBottom: 15 }}>
+        <i>Alli Karkantis - Lead Product Designer, Own Up</i>
+      </div>
+      <p>
         "Kai is the epitome of a team player. They have an ease of personality
         that allows them to get along with everyone, paired with an honest moral
         compass; they are a team member who is looking out for the good of
         everyone in the organization, and working to ensure that workplaces are
-        a safe and inclusive atmosphere for all. Kai leads by inspiring others to
-        be the best they can be and helping them to reach their goals, while
+        a safe and inclusive atmosphere for all. Kai leads by inspiring others
+        to be the best they can be and helping them to reach their goals, while
         working hard to better themselves along the way. If you have a problem,
         Kai will have a myriad of creative and thoughtful solutions. It was an
         honor and a pleasure to work with Kai, and I can say with absolute
@@ -21,15 +88,15 @@ export const WorkingWithMe = () => (
         <i>Natashia Ayres - Shipping Operations Specialist, Addgene</i>
       </div>
       <p>
-        "Working with Kai is so wonderful because they bring so many things to an
-        organization's mission and to a team: passion, commitment, a
+        "Working with Kai is so wonderful because they bring so many things to
+        an organization's mission and to a team: passion, commitment, a
         collaborative spirit, creativity, innovation, intelligence,
         perseverance, kindness, compassion. I have learned so much working with
         Kai - most outstanding is what it means to jump into something you don't
-        know, and to figure out how to achieve it. Anything that is given to Kai,
-        they tackle. They are someone I can depend on, someone I can trust. Kai is
-        also so fun to work with. All of this together makes Kai an amazing person
-        to have on your team."
+        know, and to figure out how to achieve it. Anything that is given to
+        Kai, they tackle. They are someone I can depend on, someone I can trust.
+        Kai is also so fun to work with. All of this together makes Kai an
+        amazing person to have on your team."
       </p>
       <div style={{ alignSelf: "flex-end", paddingBottom: 15 }}>
         <i>Sara Minkara - Founder & CEO, Empowerment Through Integration</i>
